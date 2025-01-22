367c7663577ec84aad8863a1938d82fd91dbb207
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2025-01-22 15:13:21 +0900

    if statements in the email_slicer function

--- Email-Slicer/email-slicer.py
+++ Email-Slicer/email-slicer.py
@@ -1,7 +1,10 @@
 # Email slicer program
 
 def email_slicer():
-    pass
+    print("Welcome to the email slicer program!")
+
+    while True:
+        email = input("Enter your email or type 'exit' to quit the program: ").strip()
 
 if __name__ == "__main__":
     email_slicer()
