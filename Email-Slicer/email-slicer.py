70546c241221dccb77bf4c1a36fd11d1d99ac70e
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2025-01-22 15:33:22 +0900

    Welcome Function completed

--- Email-Slicer/email-slicer.py
+++ Email-Slicer/email-slicer.py
@@ -1,6 +1,12 @@
 # Email slicer program
 # The email slcier program has been refactored and enhanced since I created the file in December
 
+def welcome_message():
+    print("Welcome to the email slicer program")
+    print("You can analyze emails, view statistics and more.")
+    print("Type 'exit' at any time to quit the program.")
+
+
 def email_slicer():
     print("Welcome to the email slicer program!")
 
@@ -23,5 +29,11 @@ def email_slicer():
         except ValueError as error:
             print(f"Error: {error}. Please provide a valid email address.")
 
+def email_statistics():
+    pass
+
+def main():
+    pass
+
 if __name__ == "__main__":
-    email_slicer()
+    main()
