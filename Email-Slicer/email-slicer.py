ac5922d3129a45b4f04ecf1678fd5602cf83477e
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2025-01-22 15:15:37 +0900

    Adding logic and print statements to the if statements

--- Email-Slicer/email-slicer.py
+++ Email-Slicer/email-slicer.py
@@ -6,5 +6,13 @@ def email_slicer():
     while True:
         email = input("Enter your email or type 'exit' to quit the program: ").strip()
 
+        if email.lower() == "exit":
+            print("Thank you for using the email slicer program. Bye Bye!")
+            break
+
+        if "@" not in email or email.count("@") != 1:
+            print("Invalid email address. Please try again.")
+            continue
+
 if __name__ == "__main__":
     email_slicer()
