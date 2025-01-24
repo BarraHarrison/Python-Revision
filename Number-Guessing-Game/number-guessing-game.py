0acb4615c40c5907f038004d0ad516de2e2ed6ac
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2025-01-24 13:13:02 +0900

    While True Loop breaks if the player guesses correctly

--- Number-Guessing-Game/number-guessing-game.py
+++ Number-Guessing-Game/number-guessing-game.py
@@ -18,17 +18,17 @@ def number_guessing_game():
   
         guess = int(guess)
         guesses += 1
-        if guess < lowest_number or guess > highest_number:
-            print("That number is out of range")
-            print(f"Select a number between {lowest_number} and {highest_number}")
+
+        if not (lowest_number <= guess <= highest_number):
+            print(f"Your guess is out of range. Please choose a number between {lowest_number} and {highest_number}.")
         elif guess < answer:
             print("Too low! Try again!")
         elif guess > answer:
             print("Too high! Try again!")
         else:
             print(f"CORRECT! The answer was {answer}")
-            print(f"Number of guesses: {guesses}")
-            is_running = False
-    else:
-        print("Invalid guess")
-        print(f"Select a number between {lowest_number} and {highest_number}")
+            print(f"You guessed it in {guesses} tries.")
+            break
+
+if __name__ == "__main__":
+    number_guessing_game()
